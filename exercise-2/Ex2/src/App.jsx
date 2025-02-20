eeb3213308cccba382984164752a421c1d6c7bcd
Visnuka <sophavisnukakhun@gmail.com>
2025-02-20 21:56:27 +0700

    JSX - Dynamic Data - Components

---
+++ exercise-2/Ex2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../component/Header";
+import Main from "../component/Main";
+import Footer from "../component/Footer"; 
+
+function App() { 
+  return (
+    <>
+      <p>Hello</p>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
